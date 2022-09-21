9d61ca208dc54509a84b88d22ec2df1f487a0d88
Shubhashis Roy Dipta <iamdipta@gmail.com>
2022-09-21 01:16:56 -0400

    init lib with params

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import hydra
+import torch
 
 @hydra.main(version_base=None, config_path="config", config_name="root")
 def my_app(cfg):
-    activation = hydra.utils.instantiate(cfg.train.activation)
-    print(activation, type(activation))
+    model = torch.nn.Linear(10, 2)
+    optimizer = hydra.utils.instantiate(cfg.train.optimizer)(model.parameters())
+    print(type(optimizer))
 
 if __name__ == "__main__":
     my_app()
