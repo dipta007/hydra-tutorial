d1b5f9a975ff2b5771328e02a43bde6fcb4662a3
Shubhashis Roy Dipta <iamdipta@gmail.com>
2022-09-25 15:14:31 -0400

    recursive init

--- trainer/dummy.py
+++ trainer/dummy.py
@@ -1,3 +1,4 @@
 class DummyTrainer:
-    def run(self):
-        print('Hello World!')
+    def __init__(self, dummy2):
+        self.dummy2 = dummy2
+        print('Hello World! I am dummy 1')
---
+++ trainer/dummy2.py
@@ -0,0 +1,3 @@
+class DummyTrainer2:
+    def __init__(self):
+        print('Hello World! I am dummy 2')
