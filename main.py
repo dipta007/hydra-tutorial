f438fc204faa963a1555d76da713d70be9c52638
Shubhashis Roy Dipta <iamdipta@gmail.com>
2022-09-25 15:03:23 -0400

    group config files

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import hydra
 
-@hydra.main(version_base=None, config_path="config", config_name="root")
+@hydra.main(version_base=None, config_path="config")
 def my_app(cfg):
-    print(cfg.train.enc_hid_size)
-    print(cfg['train']['enc_hid_size'])
-    print(cfg.train.dec_hid_size)
-
-    cfg.train.vocab_size = 40000
-    print(cfg.train.vocab_size)
+    print(cfg)
 
 if __name__ == "__main__":
     my_app()
